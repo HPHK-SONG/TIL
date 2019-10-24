d9ff4a075473bf37f0bcba0bdb8f248571fec784
HPHK-SONG <hphksong@gmail.com>
2019-10-24 17:32:48 +0900

    Add class example

--- ES6/class.js
+++ ES6/class.js
@@ -47,3 +47,36 @@ const 벤틀리 = new SuperCar({
 });
 벤틀리.drive();
 벤틀리.booster();
+
+// 실습 1 - RPG게임 개발 중...
+// Monster 클래스의 instance는 생성될 때, health가 100이다.
+// constructor는 options라는 인자를 받으며
+// options는 name이라는 키가 있다.{name:''}
+// Monster를 만들고 피카츄라는 이름을 가진 몬스터를 만들자.
+class Monster {
+  constructor(options) {
+    this.name = options.name;
+    this.health = 100;
+  }
+}
+const 피카츄 = new Monster({ name: "피카츄" });
+console.log(피카츄.health);
+// 실습 2
+// Monster 클래스의 자식 클래스 Dinosaur를 만들어주시되,
+// 생성자함수는 같고,
+// Dinosaur는 몸통박치기라는 메소드를 갖습니다.
+// 몸통박치기는 다른 몬스터 인스턴스를 인자로 받고,
+// 결과로 인자로 받은 몬스터의 health를 10깎습니다.
+// Tirano.몸통박치기(피카츄)
+// => console.log(피카츄.health) => 90
+class Dinosaur extends Monster {
+  constructor(options) {
+    super(options);
+  }
+  몸통박치기(monster) {
+    monster.health -= 10;
+  }
+}
+const tirano = new Dinosaur({ name: "티라노" });
+tirano.몸통박치기(피카츄);
+console.log(피카츄.health);
