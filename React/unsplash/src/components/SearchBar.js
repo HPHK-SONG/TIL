9e60ca334ef417279a7236143c94c08109735269
HPHK-SONG <hphksong@gmail.com>
2019-10-29 14:45:44 +0900

    log res

--- React/unsplash/src/components/SearchBar.js
+++ React/unsplash/src/components/SearchBar.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  onChange = e => {
+    this.props.handleKeyword(e.target.value);
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit();
+  };
+
   render() {
     return (
       <div className="ui segment container">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSubmit}>
           <label htmlFor="keyword">Search in Eng!</label>
-          <input type="text" id="keyword" />
+          <input type="text" id="keyword" onChange={this.onChange} />
         </form>
       </div>
     );
