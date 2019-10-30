f84a29aead3888dc1268e3684f5d55f96dd9b0ea
HPHK-SONG <hphksong@gmail.com>
2019-10-30 15:06:34 +0900

    Add react hooks

--- React/forecast/src/components/App.js
+++ React/forecast/src/components/App.js
@@ -3,36 +3,33 @@ import Current from "./Current";
 import Forecast from "./Forecast";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(1);
-  //   state = {
-  //       count:0,
-  //   }
+  const APPID = "988033fdda81ccb3fa6a02cb6dab9c85";
+  const [current, setCurrent] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  const getLocation = () => {
+    return new Promise((resolve, reject) => {
+      window.navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
 
+  const getTemp = async coords => {};
+  const getHourlyTemp = async coords => {};
+
+  const getAll = async () => {
+    try {
+      const { coords } = await getLocation();
+      await getTemp(coords);
+      await getHourlyTemp(coords);
+    } catch (error) {
+      alert("위치 제발 동의해주세요ㅠㅠ");
+    }
+  };
   useEffect(() => {
-    console.log("useEffect가 일어났어요!");
-    document.title = `U clicked ${count} times`;
-  }, [count, number]); //componentDidMount
+    getAll();
+  }, []);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-          //this.setState({count:this.state.count+1});
-        }}
-      >
-        카운트업!
-      </button>
-      <button
-        onClick={() => {
-          setNumber(number - 1);
-        }}
-      >
-        카운트다운!
-      </button>
-      <h2>{count}</h2>
-      <h2>{number}</h2>
       <header>
         <h1>일기예보</h1>
       </header>
