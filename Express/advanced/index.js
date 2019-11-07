fa5fb1eaf3d154f449ef52713370d59a6073a3e6
HPHK-SONG <hphksong@gmail.com>
2019-11-07 11:16:26 +0900

    add db middle ware

--- Express/advanced/index.js
+++ Express/advanced/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const user = require("./routes/user");
 const article = require("./routes/article");
 const app = express();
-
+app.use((req, res, next) => {
+  mongoose
+    .connect("mongodb://localhost/express-advanced", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true
+    })
+    .then(() => {
+      console.log("db connected!");
+      next();
+    })
+    .catch(error => next(error)); //에러를 다음애한테 던짐
+});
 app.use(express.json());
 app.use("/api/user", user);
 app.use("/api/article", article);
+app.use(() => {
+  mongoose.disconnect().then(() => console.log("db disconnected!"));
+});
 
 app.listen(3000, () => console.log("서버를 돌리고 있습니다..."));
--- Express/advanced/routes/user.js
+++ Express/advanced/routes/user.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const { User, validateUser } = require("../models/user");
 const router = express.Router();
 
-const users = [
-  { id: 1, name: "song" },
-  { id: 2, name: "kang" },
-  { id: 3, name: "kim" }
-];
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const users = await User.find();
   res.send(users);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+});
+
+router.post("/", async (req, res) => {
+  if (validateUser(req.body).error) {
+    res.status(400).send("잘못된 요청입니다");
+    return;
+  }
+  const user = new User({
+    name: req.body.name
+  });
+  const result = await user.save();
+  res.status(200).send(result);
 });
 
 module.exports = router;
